c6cdc3e52f0aad31314f430f62bc99940fa137f8
VictoriaMS <vikamag48@gmail.com>
2021-11-23 18:36:37 +0600

    Lesson 2 homework 2

---
+++ Lesson 2/homework_2.rb
@@ -0,0 +1,18 @@
+array= []
+number = 10 
+while number <= 100 do 
+    array << number 
+    number += 5
+end 
+
+
+#второй способ решения задачи 
+array = (10..100).to_a
+array.delete_if {|number| number % 5 != 0}
+
+
+#третий способ решения задачи 
+array = []
+for number in (10..100) do 
+    array.push(number) if number % 5 == 0
+end
