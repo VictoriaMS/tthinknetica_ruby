64891e97d55e45c6323adac12c0be017ff2315b3
VictoriaMS <vikamag48@gmail.com>
2021-11-23 14:29:30 +0600

    lesson 1 homework 1

---
+++ Lesson 1/homework_1.rb
@@ -0,0 +1,13 @@
+puts "What is your name?"
+name = gets.chomp 
+
+puts "How tall are you?"
+height = gets.chomp.to_i
+
+ideal_weight = height - 110 
+
+if ideal_weight < 0 
+    puts "Your weight is already optimal"
+else 
+    puts "#{name}, your ideal weight is #{ideal_weight}"
+end
