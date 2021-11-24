7e34f8bfd508d083bec4376798b2d8014c807847
VictoriaMS <vikamag48@gmail.com>
2021-11-24 10:47:14 +0600

    some changes

--- Lesson 2/homework_2.rb
+++ Lesson 2/homework_2.rb
@@ -1,5 +1,6 @@
-array= []
+array = []
 number = 10 
+
 while number <= 100 do 
     array << number 
     number += 5
@@ -13,6 +14,7 @@ array.delete_if {|number| number % 5 != 0}
 
 #третий способ решения задачи 
 array = []
+
 for number in (10..100) do 
     array.push(number) if number % 5 == 0
 end
--- Lesson 2/homework_3.rb
+++ Lesson 2/homework_3.rb
@@ -1,5 +1,7 @@
 array = [0, 1]
+
 sum_two_numbers = array[-1] + array[-2]
+
 while sum_two_numbers < 100 do 
     array << sum_two_numbers
     sum_two_numbers = array[-1] + array[-2]
