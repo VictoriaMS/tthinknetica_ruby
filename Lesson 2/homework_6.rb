64ffaebe4d748e7d66d5d4c0de06600cd2b79f9e
VictoriaMS <vikamag48@gmail.com>
2021-11-24 11:43:49 +0600

    Lesson 2 homework 6

---
+++ Lesson 2/homework_6.rb
@@ -0,0 +1,20 @@
+products = {}
+total_sum = 0 
+
+loop do 
+    puts "Product name: "
+    product_name = gets.chomp 
+
+    break if product_name == "Stop"    
+
+    puts "Price: "
+    price = gets.chomp.to_f
+    puts "Number of products: "
+    number = gets.chomp.to_f
+
+    products[product_name] = { price: price, number: number, total: price * number }
+end 
+
+products.each_value{ |values| total_sum += values[:total] }
+
+puts total_sum 
