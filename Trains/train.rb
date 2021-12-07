2aa7d31721d1692820f35bfd048f519f4deb0e3b
VictoriaMS <vikamag48@gmail.com>
2021-12-07 18:46:07 +0600

    changes by comments

--- Trains/train.rb
+++ Trains/train.rb
@@ -1,5 +1,5 @@
 class Train 
-  attr_accessor :speed, :count_vagons, :route
+  attr_accessor :speed, :count_vagons, :route, :station_index
   attr_reader :type 
 
   def initialize(number, type, count_vagons)
@@ -7,7 +7,6 @@ class Train
     @type = type 
     @count_vagons = count_vagons
     @speed = 0
-    @route = nil
   end 
   
   def pick_up_speed
@@ -19,7 +18,7 @@ class Train
   end
   
   def attach_vagon 
-    if speed == 0 
+    if speed.zero?
       self.count_vagons += 1
     else 
       puts 'you cannot attach a vagon while the train is moving'
@@ -27,8 +26,8 @@ class Train
   end 
   
   def unhook_vagon
-    if speed == 0 
-      self.count_vagons -= 1
+    if speed.zero? && count_vagons != 0
+      self.count_vagons -= 1 
     else 
       puts 'you cannot unhook a vagon while the train is moving'
     end
@@ -36,38 +35,31 @@ class Train
    
   def route=(route)
     @route = route 
-    route.stations.first.accept_train(self)
+    @station_index = 0
   end
 
   def current_station
-    route.stations.select{ |station| station.trains.include?(self) }[0]
+    route.stations[station_index]
   end
   
   def next_station 
-  index_current_station + 1 >= route.stations.size ? route.stations[0] : route.stations[index_current_station + 1]
+    route.stations[station_index + 1]
 
   end 
   
   def previous_station
-    route.stations[index_current_station- 1]
+    route.stations[station_index - 1] if station_index != 0 
   end 
   
   def moving_forward 
-    station = current_station
-    next_station.accept_train(self)
-    station.send_train(self)
+    if next_station
+      current_station.send_train(self)
+      next_station.accept_train(self)
+    end
   end
 
   def moving_back
-    station = current_station
+    current_station.send_train(self)
     previous_station.accept_train(self)
-    station.send_train(self)
   end
-
-  private 
-
-  def index_current_station
-    route.stations.index(current_station)
-  end 
-
 end 
